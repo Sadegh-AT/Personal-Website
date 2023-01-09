d0d7b2d93633e5254a5fab7e4a4475f3e46b67e2
SadeghAT <sadegh.artivox@gmail.com>
2023-01-10 00:17:53 +0330

    add Color to tailwind, make header html code and styling with tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,29 @@
 module.exports = {
     content: ["./src/js/*.{html,js}", "./src/index.html"],
     theme: {
-        extend: {},
+        screens: {
+            sm: "576px",
+            md: "768px",
+            lg: "992px",
+            xl: "1200px",
+            xxl: "1400px",
+        },
+        extend: {
+            colors: {
+                textcolor: "#333333",
+                shadowcolor: "rgba(4,195,255,0.15)",
+                appbg: "#fff",
+                sectionbgcolor: "#fff",
+                AmericanGreen: "#49AF46",
+                SpiroDiscoBall: "#13B6FF",
+                PortlandOrange: "#FF6331",
+                CerisePink: "#EC407A",
+                MikadoYellow: "#FFC50C",
+                LavenderIndigo: "#8540FF",
+                PrincetonOrange: "#F57F17",
+                TiffanyBlue: "#0ABEAD",
+            },
+        },
     },
     plugins: [],
 };
