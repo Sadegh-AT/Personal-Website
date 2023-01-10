09a4d9d046c11cfe51fbeafc6fb620aefbb8926e
SadeghAT <sadegh.artivox@gmail.com>
2023-01-10 18:49:51 +0330

    Complete header

--- src/js/nav-icon.js
+++ src/js/nav-icon.js
@@ -1,5 +1,7 @@
 let ico = document.querySelector("#nav-icon3");
-let menu = document.querySelector(".menu-open")
-ico.addEventListener("click",function(){
-    this.classList.toggle("open")
-})
+let menu = document.querySelector(".menu-open");
+ico.addEventListener("click", function () {
+  this.classList.toggle("open");
+  menu.classList.toggle("active");
+  document.body.classList.toggle("oh");
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,29 +1,36 @@
 module.exports = {
-    content: ["./src/js/*.{html,js}", "./src/index.html"],
-    theme: {
-        screens: {
-            sm: "576px",
-            md: "768px",
-            lg: "992px",
-            xl: "1200px",
-            xxl: "1400px",
-        },
-        extend: {
-            colors: {
-                textcolor: "#333333",
-                shadowcolor: "rgba(4,195,255,0.15)",
-                appbg: "#fff",
-                sectionbgcolor: "#fff",
-                AmericanGreen: "#49AF46",
-                SpiroDiscoBall: "#13B6FF",
-                PortlandOrange: "#FF6331",
-                CerisePink: "#EC407A",
-                MikadoYellow: "#FFC50C",
-                LavenderIndigo: "#8540FF",
-                PrincetonOrange: "#F57F17",
-                TiffanyBlue: "#0ABEAD",
-            },
-        },
+  content: ["./src/js/*.{html,js}", "./src/index.html"],
+  theme: {
+    screens: {
+      sm: "576px",
+      md: "768px",
+      lg: "992px",
+      xl: "1200px",
+      xxl: "1400px",
     },
-    plugins: [],
+    extend: {
+      colors: {
+        textcolor: "#333333",
+        shadowcolor: "rgba(4,195,255,0.15)",
+        appbg: "#fff",
+        sectionbgcolor: "#fff",
+        AmericanGreen: "#49AF46",
+        SpiroDiscoBall: "#13B6FF",
+        PortlandOrange: "#FF6331",
+        CerisePink: "#EC407A",
+        MikadoYellow: "#FFC50C",
+        LavenderIndigo: "#8540FF",
+        PrincetonOrange: "#F57F17",
+        TiffanyBlue: "#0ABEAD",
+      },
+      fontFamily: {
+        Poppins: "Poppins",
+        PoppinsMedium: "Poppins Medium",
+        PoppinsSemiBold: "Poppins SemiBold",
+        PoppinsBold: "Poppins Bold",
+        PoppinsBlack: "Poppins Black",
+      },
+    },
+  },
+  plugins: [],
 };
